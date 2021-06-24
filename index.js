a7fa437f205ebff9fd07cae9afb594ff48c033a6
KoCus84 <ko.cusveller@gmail.com>
2021-06-24 19:48:08 +0200

    deployment prep

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const app = express();
+const PORT = process.env.PORT || 4000;
 
 const corsMiddleWare = require("cors");
 app.use(corsMiddleWare());
 
 const bodyParserMiddleWare = express.json();
 app.use(bodyParserMiddleWare);
-
-const PORT = 4000;
 
 const projectsRouter = require("./routers/projects");
 app.use("/", projectsRouter);
