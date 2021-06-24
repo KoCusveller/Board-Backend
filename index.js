76708b3c7515a85008b42d6590ebbfba7e45802f
KoCus84 <ko.cusveller@gmail.com>
2021-06-24 14:34:46 +0200

    Pebble routes

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const app = express();
+
+const corsMiddleWare = require("cors");
+app.use(corsMiddleWare());
+
+const bodyParserMiddleWare = express.json();
+app.use(bodyParserMiddleWare);
+
 const PORT = 4000;
 
 const projectsRouter = require("./routers/projects");
 app.use("/", projectsRouter);
+
+const pebblesRouter = require("./routers/pebbles");
+app.use("/board", pebblesRouter);
 
 app.listen(PORT, () => {
 	console.log(`Listening on port: ${PORT}`);
