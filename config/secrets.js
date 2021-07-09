4547de7ed0b7f2a6d68b2521bb546070b6fe55a8
KoCus84 <ko.cusveller@gmail.com>
2021-07-09 13:22:08 +0200

    login/logout functionality

---
+++ config/secrets.js
@@ -0,0 +1,4 @@
+module.exports = {
+	jwtSecret:
+		process.env.JWT_SECRET || "e9rp^&^*&@9sejg)DSUA)jpEds8394jdsfn,m",
+};
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 4000;
+const authRouter = require("./routers/auth");
+const authMiddleWare = require("./auth/middleware");
 
 const corsMiddleWare = require("cors");
 app.use(corsMiddleWare());
@@ -13,6 +15,8 @@ app.use("/", projectsRouter);
 
 const pebblesRouter = require("./routers/pebbles");
 app.use("/board", pebblesRouter);
+
+app.use("/", authRouter);
 
 app.listen(PORT, () => {
 	console.log(`Listening on port: ${PORT}`);
